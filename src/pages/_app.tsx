7947847d626ddb8cfa9e9de8718e2ee262efc491
wellysonvie <wellysonvie@gmail.com>
2021-04-20 17:54:07 -0300

    feat: criando player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
-import { Header } from '../components/Header';
 
 import '../styles/global.scss'
 
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+import React from 'react';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
     </div>
   );
 }
