248478c6112111fad3f906c24a6b3242029b8f02
wellysonvie <wellysonvie@gmail.com>
2021-04-25 17:23:18 -0300

    feat: controlar progresso do player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { usePlayer } from '../../contexts/PlayerContext';
 import Slider from 'rc-slider';
+
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 import 'rc-slider/assets/index.css';
 
 import styles from './styles.module.scss';
 
+
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [progress, setProgress] = useState(0);
 
   const {
     episodeList,
@@ -23,7 +27,8 @@ export function Player() {
     hasPrevious,
     hasNext,
     playNext,
-    playPrevious
+    playPrevious,
+    clearPlayerState
   } = usePlayer();
 
   useEffect(() => {
@@ -37,6 +42,27 @@ export function Player() {
       audioRef.current.pause();
     }
   }, [isPlaying]);
+
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0;
+
+    audioRef.current.addEventListener('timeupdate', () => {
+      setProgress(Math.floor(audioRef.current.currentTime));
+    });
+  }
+
+  function handleSeek(amount: number) {
+    audioRef.current.currentTime = amount;
+    setProgress(amount);
+  }
+
+  function handleEpisodeEnded() {
+    if (hasNext) {
+      playNext();
+    } else {
+      clearPlayerState();
+    }
+  }
 
   const episode = episodeList[currentEpisodeIndex];
 
@@ -66,10 +92,13 @@ export function Player() {
 
       <footer className={!episode ? styles.empty : ""}>
         <div className={styles.progress}>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(progress)}</span>
           <div className={styles.slider}>
             {episode ? (
               <Slider
+                max={episode.duration}
+                value={progress}
+                onChange={handleSeek}
                 trackStyle={{ backgroundColor: '#04d361' }}
                 railStyle={{ backgroundColor: '#9f75ff' }}
                 handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
@@ -78,7 +107,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             }
           </div>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
         {episode && (
@@ -87,7 +116,9 @@ export function Player() {
             ref={audioRef}
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
+            onEnded={handleEpisodeEnded}
             loop={isLooping}
+            onLoadedMetadata={setupProgressListener}
             autoPlay
           />
         )}
--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -24,6 +24,7 @@ type PlayerContextData = {
   hasNext: boolean;
   playNext: () => void;
   playPrevious: () => void;
+  clearPlayerState: () => void;
 };
 
 export const PlayerContext = createContext({} as PlayerContextData);
@@ -67,8 +68,13 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     setIsPlaying(state);
   }
 
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
   const hasPrevious = currentEpisodeIndex > 0;
-  const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
+  const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length;
 
   function playNext() {
     if (isShuffling) {
@@ -102,7 +108,8 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         hasPrevious,
         hasNext,
         playNext,
-        playPrevious
+        playPrevious,
+        clearPlayerState
       }}>
 
       {children}
